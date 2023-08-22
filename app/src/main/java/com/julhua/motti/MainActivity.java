664327531ea7d6920fbfc78431fab675a67bce4c
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2023-08-22 17:11:12 -0500

    Creacion del proyecto en android

---
+++ app/src/main/java/com/julhua/motti/MainActivity.java
@@ -0,0 +1,14 @@
+package com.julhua.motti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
