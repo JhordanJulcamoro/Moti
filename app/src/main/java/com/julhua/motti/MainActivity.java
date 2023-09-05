f6795b9cca6634253dc16a40fef0a47486a4a7a0
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2023-09-04 19:19:34 -0500

    update 04-09

--- app/src/main/java/com/julhua/motti/MainActivity.java
+++ app/src/main/java/com/julhua/motti/MainActivity.java
@@ -2,13 +2,39 @@ package com.julhua.motti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button mButtonIAmClient, mButtonIAmDriver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonIAmClient = findViewById(R.id.btnIAmClient);
+        mButtonIAmDriver = findViewById(R.id.btnIAmDriver);
+
+        mButtonIAmClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSelectAuth();
+            }
+        });
+
+        mButtonIAmDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSelectAuth();
+            }
+        });
+    }
+
+    private void goToSelectAuth() {
+        Intent intent = new Intent(MainActivity.this, SelectOptionAuthActivity.class);
+        startActivity(intent);
     }
 }
